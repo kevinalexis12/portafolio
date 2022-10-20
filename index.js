88691c30070639caf370e59ad98be6d80ef055a0
kevin <sans111320@gmail.com>
2022-10-19 23:20:21 -0500

    modificaciones portafolio

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express =require('express' )
+const res = require('express/lib/response')
 const app = express()
-const port=3000
+const port= 3000
 const path =require('path')
 app.use('/static',express.static(path.join(__dirname,'public')))
 //app.use('/static',express.static('public'))
